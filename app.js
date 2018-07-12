e7a67970e06c325c44822614951061d322c33579
MichelleTW <michelle150810@gmail.com>
2018-07-13 06:06:27 +0800

    1-add constructor 3筆初始todolists

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // 建構子，每個 class 第一次產生時都會執行到這邊
+  constructor (props) {
+    super(props);
+
+    // 設定 state
+    this.state = {
+      todos: [
+        {id: 1, name: 'hello', completed: false},
+        {id: 2, name: 'aaaaaa', completed: true},
+        {id: 3, name: 'world', completed: false}
+      ]
+    }
+  }
   render() {
+
+    // 從 state 取出資料
+    let todos = this.state.todos;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          {
+            todos.map((todo) => {
+
+              // 傳回 jsx
+              return (
+                <li>
+                  name:{todo.name}, {todo.completed ? '已完成' : ''}
+                </li>
+              );
+            })
+          }
+        </ul>
       </div>
     );
   }
